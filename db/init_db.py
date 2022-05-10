e627955da6035a29b46a8ef6ac841be5deed12c2
hnguyen1910 <93813020+hnguyen1910@users.noreply.github.com>
2022-05-10 21:11:27 +0700

    Update init_db.py

--- db/init_db.py
+++ db/init_db.py
@@ -2,7 +2,7 @@ import aiosqlite
 import asyncio
 
 async def init():
-	async with aiosqlite.connect("url.db") as db:
+	async with aiosqlite.connect("name.db") as db:
 		with open("db/init_db.sqlite") as f:
 			await db.executescript(f.read())
 		await db.commit()
